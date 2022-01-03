022de33343a1d5a59ab3bcee5bdaf67cf28e2182
McCray <tubach75@hotmail.com>
2022-01-03 10:52:10 -0500

    checked status of counterReducer and added notes

--- redux-documentation/src/app/store.js
+++ redux-documentation/src/app/store.js
@@ -6,3 +6,8 @@ export const store = configureStore({
     counter: counterReducer,
   },
 });
+/*
+1) ConfigureStore takes a reducer object as an argument.
+2) The counterReducer entity appears to not care what its name is.  It appears to be the
+export default couterSlice.reducer from counterSlice.js.
+*/
