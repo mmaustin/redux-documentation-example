22e0945a613a5b3a9abfbdb9187a41aa05a9b1eb
McCray <tubach75@hotmail.com>
2022-01-04 09:01:02 -0500

    tested incrementAmount without the Number object

--- redux-documentation/src/features/counter/Counter.js
+++ redux-documentation/src/features/counter/Counter.js
@@ -16,7 +16,7 @@ export function Counter() {
   const [incrementAmount, setIncrementAmount] = useState('2');
 
   const incrementValue = Number(incrementAmount) || 0;
-
+  
   return (
     <div>
       <div className={styles.row}>
