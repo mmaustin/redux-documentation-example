2755c3342f28ccd6df64043db7dcf3cf23bfa68d
McCray <tubach75@hotmail.com>
2022-01-05 15:59:02 -0500

    implemented useSelector in questions list component

--- redux-documentation/src/features/questions/QuestionsList.js
+++ redux-documentation/src/features/questions/QuestionsList.js
@@ -1,7 +1,13 @@
-import React, { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import React from 'react' //import React, { useState } from 'react';
+import { useSelector} from 'react-redux';
+import { selectQuestions } from './questionsSlice';
 
 export const QuestionsList = () => {
+
+    const questions = useSelector(selectQuestions)
+
+    
+
     return(
         <>
         This will be where the questions go.
