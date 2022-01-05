a500fddc8e7c0a705b6dd09bc09986b75e35bd63
McCray <tubach75@hotmail.com>
2022-01-05 13:05:46 -0500

    imported Questions component to App.js

--- redux-documentation/src/App.js
+++ redux-documentation/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
+import { Questions } from './features/questions/Questions';
 import './App.css';
 
 function App() {
@@ -51,6 +52,9 @@ function App() {
           </a>
         </span>
       </header>
+      <section className="Questions">
+        <Questions />
+      </section>
     </div>
   );
 }
