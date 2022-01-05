5cb3bcb1a356fe65a6c36ba22142d6d7162a472c
McCray <tubach75@hotmail.com>
2022-01-05 15:31:22 -0500

    added questions reducer to the redux store

--- redux-documentation/src/app/store.js
+++ redux-documentation/src/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import questionsReducer from '../features/questions/questionsSlice';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    questions: questionsReducer
   },
 });
 /*
