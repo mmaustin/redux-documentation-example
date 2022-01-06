296a7e003ab820b64849d4e6d1a85863b996e51f
McCray <tubach75@hotmail.com>
2022-01-06 13:27:06 -0500

    deleted comments from redux store

--- redux-documentation/src/app/store.js
+++ redux-documentation/src/app/store.js
@@ -8,8 +8,3 @@ export const store = configureStore({
     questions: questionsReducer
   },
 });
-/*
-1) ConfigureStore takes a reducer object as an argument.
-2) The counterReducer entity appears to not care what its name is.  It appears to be the
-export default couterSlice.reducer from counterSlice.js.
-*/
