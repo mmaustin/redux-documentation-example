6b6583aebc0c754a76e5208b0f98707afabcaf82
McCray <tubach75@hotmail.com>
2022-01-06 13:45:07 -0500

    added useDispatch to questions list component

--- redux-documentation/src/features/questions/QuestionsList.js
+++ redux-documentation/src/features/questions/QuestionsList.js
@@ -1,5 +1,5 @@
 import React from 'react' //import React, { useState } from 'react';
-import { useSelector} from 'react-redux';
+import { useSelector, useDispatch} from 'react-redux';
 import { selectQuestions } from './questionsSlice';
 
 export const QuestionsList = () => {
