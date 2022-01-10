9c5959ee38c75e4dea3886545e01641a853057d9
McCray <tubach75@hotmail.com>
2022-01-10 16:59:13 -0500

    debugging the invalid hook call error

--- redux-documentation/src/features/questions/QuestionsList.js
+++ redux-documentation/src/features/questions/QuestionsList.js
@@ -30,7 +30,7 @@ export const QuestionsList = () => {
             <div>
                 {renderedQuestions}
             </div>
-            <button onClick={() => dispatch(pushed('Let\'s pretend that this is a question'))}>Push Push Push</button>
+            <button onClick={() => dispatch(pushed({id: nanoid, question: 'second question', answer: 'second answer'}))}>Push Push Push</button>
         </section>
     )
 }
